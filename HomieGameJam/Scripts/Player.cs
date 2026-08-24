635761f040b06a775d04bd5de32755f0d653e49c
MrBones98 <salasdaniel1998@gmail.com>
2026-08-24 18:33:44 +0200

    Rework Controller

--- HomieGameJam/Scripts/Player.cs
+++ HomieGameJam/Scripts/Player.cs
@@ -9,9 +9,9 @@ public class Player : KinematicBody2D
 	[Export]
 	public int Speed {get; set;} = 100; // How fast the player will move (pixels/sec).
 	[Export]
-	public float JumpSpeed {get; set;} = -500; 
-
-	public const float Gravity = 5000; 
+	public float JumpSpeed {get; set;} = 500; 
+	[Export]
+	public int Gravity; 
 
 	private Vector2 _velocity = new Vector2(); // The player's movement vector.
 
@@ -29,46 +29,8 @@ public class Player : KinematicBody2D
 
 
 	public override void _Process(float delta)
-	{	 
-	}
-
-	public override void _PhysicsProcess(float delta)
 	{
-		
-
-		if(!IsOnFloor())
-		{
-			_velocity.y += Gravity * (float)delta;
-		}
-		else
-		{
-			if (Input.IsActionPressed("move_right"))
-			{
-			_velocity.x += Speed;
-			}
-			else if (Input.IsActionPressed("move_left"))
-			{
-			_velocity.x -= Speed;
-			}
-
-			if (Input.IsActionJustPressed("jump"))
-			{
-			_velocity.y = JumpSpeed;
-			}
-			
-		}
-
-		
-		
-		
-		
-
-
-		/* Position += _velocity * (float)delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
-			y: Mathf.Clamp(Position.y, 0, ScreenSize.y)
-		); */
+		GD.Print(IsOnFloor());
 
 		if (_velocity.x != 0)
 		{
@@ -84,10 +46,83 @@ public class Player : KinematicBody2D
 		{
 			_animatedSprite.Play("idle");
 		}
-		
-		//MoveAndCollide(_velocity * (float)delta);
-		MoveAndSlide(_velocity, Vector2.Up);
+	}
 
+	public override void _PhysicsProcess(float delta)
+	{
+		
+
+		if(!IsOnFloor())
+		{
+			//_velocity.y += Gravity * (float)delta;
+			_velocity.x = 0;
+		}
+		else
+		{
+			GD.Print("On Floor");
+			_velocity.y = 0;
+			if (Input.IsActionPressed("move_right"))
+			{
+			_velocity.x += 1;
+			}
+			else if (Input.IsActionPressed("move_left"))
+			{
+			_velocity.x -= 1;
+			}
+			else
+			{
+				_velocity.x = 0;
+			}
+
+
+			if (Input.IsActionJustPressed("jump"))
+			{
+				//_velocity.y = -JumpSpeed;
+				MoveAndCollide(new Vector2(0, -JumpSpeed));
+				
+			}
+			
+			
+		}
+
+
+
+
+
+
+
+		/* Position += _velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
+			y: Mathf.Clamp(Position.y, 0, ScreenSize.y)
+		); */
+
+		// if (_velocity.x != 0)
+		// {
+		// 	_animatedSprite.Animation = "walk";
+		// 	_animatedSprite.FlipV = false;
+		// 	_animatedSprite.FlipH = _velocity.x < 0;
+		// }
+		// else if (_velocity.y != 0)
+		// {
+		// 	_animatedSprite.Animation = "fall";
+		// }
+		// else
+		// {
+		// 	_animatedSprite.Play("idle");
+		// }
+		if (IsOnFloor())
+		{
+			_velocity  = _velocity.Normalized() * Speed;			
+		}
+		else
+		{
+			_velocity.x = 0;
+			_velocity.y += Gravity * (float)delta;
+		}
+		//MoveAndCollide(_velocity * (float)delta);
+		MoveAndSlide(_velocity, Vector2.Up,infiniteInertia:false);
+		GD.Print(_velocity);
 		
 
 	}
